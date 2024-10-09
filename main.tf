25e73662d3f5fd16364542b8b50e9b6bc24a2e8b
Nicholas Perry <122832986+naperry2011@users.noreply.github.com>
2024-10-08 23:23:28 -0700

    Update main.tf
    Needed to add some corrections

--- main.tf
+++ main.tf
@@ -16,7 +16,8 @@ resource "aws_vpc" "my_vpc" {
 
 # Subnets: Defines both public and private subnets within the VPC
 resource "aws_subnet" "public_subnets" {
-  vpc_id = aws_vpc.my_vpc
+  vpc_id = aws_vpc.my_vpc.id
+
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"
   tags = {
@@ -27,7 +28,7 @@ resource "aws_subnet" "public_subnets" {
 
 # Security Groups: Defines the security group settings
 resource "aws_subnet" "private_subnet" {
-  vpc_id = aws_vpc.my_vpc
+  vpc_id = aws_vpc.my_vpc.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-1a"
   tags = {
@@ -36,20 +37,20 @@ resource "aws_subnet" "private_subnet" {
 }
 
 resource "aws_security_group" "web_sg" {
-  vpc_id = aws_vpc.my_vpc
+  vpc_id = aws_vpc.my_vpc.id
 
-  ingress = {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_block = ["0.0.0.0/0"]
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress = {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_block = ["0.0.0.0/0"]
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -58,19 +59,20 @@ resource "aws_security_group" "web_sg" {
 }
 
 
+
 # EC2 Instance: Creates an EC2 instance using Ubuntu and places it in the public subnet
 resource "aws_instance" "web_server" {
   ami = "ami-0866a3c8686eaeeba"
   instance_type = "t2.micro"
-  subnet_id = aws_subnet.public_subnets
-  security_groups = [ aws_security_group.web_sg ]
+  subnet_id = aws_subnet.public_subnets.id
+  security_groups = [ aws_security_group.web_sg.id ]
 
   tags = {
     Name = "Web_Server"
   }
 }
 
-# Outputs: the public IP address of the created EC2 instance for easy access
+# Outputs: the public IP address of the created EC2 instance for easy
 output "instance_public_ip" {
   value = "aws_instance.web_server.public_ip"
 }
